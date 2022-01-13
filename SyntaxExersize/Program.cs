cb599a43eecbb1fcd435e4c4f1164a09ea1bba8b
matt21ferg <matt21ferguson@gmail.com>
2022-01-13 13:37:49 -0600

    frist commit

---
+++ SyntaxExersize/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SyntaxExersize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? "is less than 9" : "is more than 9";
+            Console.WriteLine($"{answer} {response}");
+
+            
+        
+        }   
+    }
+}
